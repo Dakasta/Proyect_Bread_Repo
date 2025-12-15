704d2650d3a8971a8672e2a1dff7f75a2d938248
Hectornado123 <Hecalbgon1@gmail.com>
2025-12-15 20:14:28 +0100

    a

--- 2D_Bread_/Assets/_A_Lonely_Cave/Scripts/Player.cs
+++ 2D_Bread_/Assets/_A_Lonely_Cave/Scripts/Player.cs
@@ -1,74 +1,31 @@
+using System.Collections;
+using System.Collections.Genetic;
 using UnityEngine;
-using UnityEngine.InputSystem;
-public class PlayerController2D : MonoBehaviour
+
+public class Player : MonoBehaviour
 {
+
+
+    private RigidBody2D playerRB;
+    private Animator anim;
+    private float horizontalInput;
+
+
+
+    public float speed;
+    public float jumpForce;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-
-
-    [Header("Movement And Jump Configuration")]
-    [SerializeField] float Speed = 6;
-    [SerializeField] float Jumpforce = 6;
-    [SerializeField] bool IsGrounded;
-    [SerializeField] float GroundCheckRadius;
-    [SerializeField] LayerMask GroundLayer;
-
-
-    [SerializeField] Transform Groundcheck;//referencia posicion suelo
-    Rigidbody2D playerRb;
-    Animator anim;
-    PlayerInput input;
-    Vector2 moveInput;
-
-    private void Awake()
-    {
-        playerRb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-        input = GetComponent<PlayerInput>();
-    }
     void Start()
     {
 
-    }
-    private void FixedUpdate()
-    {
-        Movement();
+        playerRB = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        
     }
 
-    void Movement()
-    {
-
-        playerRb.linearVelocity = new Vector2(moveInput.x * Speed, playerRb.linearVelocity.y);
-    }
     // Update is called once per frame
     void Update()
     {
-
+        
     }
-
-
-
-    #region impuf methods
-    public void OnMove(InputAction.CallbackContext context)
-    {
-
-        moveInput = context.ReadValue<Vector2>();
-
-    }
-    public void OnJump(InputAction.CallbackContext context)
-    {
-
-
-
-    }
-    public void OnAtack(InputAction.CallbackContext context)
-    {
-
-
-
-    }
-
-    #endregion
-
-
-
 }
