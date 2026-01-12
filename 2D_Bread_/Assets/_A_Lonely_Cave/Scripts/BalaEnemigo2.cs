6cd40e898c1b28ff60cf47925c4f55683d365625
Hectornado123 <114616054+Hectornado123@users.noreply.github.com>
2026-01-12 11:53:14 +0100

    a

---
+++ 2D_Bread_/Assets/_A_Lonely_Cave/Scripts/BalaEnemigo2.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class BalaEnemigo2 : MonoBehaviour
+{
+    public float speed = 8f;
+    private Rigidbody2D rb;
+    private Transform cubeTarget;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        rb.linearVelocity = direction.normalized * speed;
+
+        Destroy(gameObject, 4f);
+    }
+
+    public void SetTarget(Transform target)
+    {
+        cubeTarget = target;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+
+        if (collision.CompareTag("enemigo"))
+        {
+            Destroy(gameObject);
+        }
+        // Si la espada golpea la bola
+        if (collision.CompareTag("Sword"))
+        {
+            if (cubeTarget != null)
+            {
+                Vector2 directionToCube =
+                    (cubeTarget.position - transform.position).normalized;
+
+                SetDirection(directionToCube);
+
+                Destroy(gameObject, 5f);
+
+
+            }
+
+
+        }
+    }
+}
---
+++ 2D_Bread_/Assets/_A_Lonely_Cave/Scripts/Torreta1.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+
+public class Torreta1 : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float speed = 2f;
+
+    [Header("Patrol Points")]
+    [SerializeField] private Transform topPoint;
+    [SerializeField] private Transform bottomPoint;
+
+    private Vector3 targetPosition;
+
+
+
+    public GameObject projectilePrefab;
+    public float shootInterval = 1.5f;
+    public Transform firePoint;
+    private Rigidbody2D rb;
+
+
+
+
+    void Start()
+    {
+        targetPosition = topPoint.position;
+        InvokeRepeating(nameof(Shoot), 1f, shootInterval);
+    }
+
+
+    void Shoot()
+    {
+        GameObject proj = Instantiate(
+            projectilePrefab,
+            firePoint.position,
+            Quaternion.identity
+        );
+
+        BalaEnemigo2 projectile = proj.GetComponent<BalaEnemigo2>();
+
+        Vector2 direction = Vector2.left;
+
+        projectile.SetTarget(transform);
+        projectile.SetDirection(direction);
+
+    }
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            targetPosition,
+            speed * Time.deltaTime
+        );
+
+        if (Vector3.Distance(transform.position, targetPosition) < 0.05f)
+        {
+            targetPosition = targetPosition == topPoint.position
+                ? bottomPoint.position
+                : topPoint.position;
+        }
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        if (topPoint == null || bottomPoint == null) return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(topPoint.position, bottomPoint.position);
+
+        Gizmos.DrawSphere(topPoint.position, 0.1f);
+        Gizmos.DrawSphere(bottomPoint.position, 0.1f);
+    }
+#endif
+}
